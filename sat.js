a678c5c437400e78e379c3dc29ea680db12bd1db
Pooja Vikhe <Pooja Vikhe>
2016-12-17 17:43:07 +0530

    added code

---
+++ sat.js
@@ -0,0 +1,8 @@
+var os = require('os');
+var tomb = function(f){
+    return(math.round((f/1024/)*100)/100);
+    }
+    console.log('host: ' + os.hostname());
+    console.log('15 min.load average: ' + od.loadavg()[2]);
+    console.log(toMB(os.freemem))+'of' + toMB(os.totalmem()) = 'mbfree');
+    
